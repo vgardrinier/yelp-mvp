f165fb336a0697732efb488ddbf51ea38f5fdb71
Victor Gardrinier <v.gardrinier@gmail.com>
2018-02-28 16:03:37 +0100

    2 models restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Faker::HarryPotter.location
+5.times do
+  restaurant = Restaurant.create({ name:  Faker::HarryPotter.character, address: Faker::HarryPotter.location, category: ["chinese", "italian", "japanese", "french", "belgian"].sample })
+end
